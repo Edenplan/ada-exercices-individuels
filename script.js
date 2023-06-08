9b9c12a03a9457aa28e8159deaede9064550a335
Edenplan <edenplanelle@gmail.com>
2023-06-08 10:07:57 +0200

    first

---
+++ script.js
@@ -0,0 +1,13 @@
+// const nm = prompt( "Quel est ton nom ?")
+// let a = `Bonjour ${nm}`
+// document.body.innerHTML += `<h2> ${a} </h2>` 
+// const ag = prompt( "Quelle est ton année de naissance ?")
+// let c = 2023
+// let b = c-Number(ag)
+// document.body.innerHTML += `<h3> Vous avez ${b} ans </h3>`
+
+function askName() {const nm = prompt( "Quel est ton nom ?")
+let a = `Bonjour ${nm}`
+document.body.innerHTML += `<h2> ${a} </h2>`
+} askName(); 
+
